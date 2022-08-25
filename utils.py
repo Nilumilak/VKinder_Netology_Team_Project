e84b2095d721c7b753a18c7a20e9aae30a39fdc4
Nikolay <nikolay310585@yandex.ru>
2022-08-25 17:28:29 +0300

    add function top_3_photos

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from vkbottle import API
 from vk_user import VkUser
 import configparser
+import asyncio
 
 
 config = configparser.ConfigParser()
@@ -34,4 +35,21 @@ async def top_3_photos(option_id: int) -> list:
     :param: option_id: vk user id.
     :return: top profile photos
     """
-    ...
+    photos = await api.photos.get_all(owner_id=option_id, count=200, extended=1)
+    
+    like_list = []
+    max_count_like_list = []
+    photos_list= []  
+
+    for photo in photos.items:
+        like_list.append(photo.likes.count) 
+
+    like_list.sort()
+    max_count_like_list += like_list[-3:]
+
+    for photo in photos.items:
+        if photo.likes.count in max_count_like_list:
+            photos_list.append(f'photo{option_id}_{photo.id}')                   
+    return photos_list
+    
+asyncio.run(top_3_photos(1))
