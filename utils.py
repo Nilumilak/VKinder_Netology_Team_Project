04b65040aacc2636f290cbc90ab90c8d0b471887
Молдованов Николай Николаевич <MoldovanovNN@alrosa.ru>
2022-08-30 14:02:06 +0300

    add moduls utils, vk_user

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from vkbottle import API
-from vk_user import VkUser
+from vk_user import VkUser, user_vk
 import configparser
+# import asyncio
 
 
 config = configparser.ConfigParser()
@@ -10,13 +11,28 @@ user_token = config['VK']['user_token']
 api = API(token=user_token)
 
 
-async def show_option(vk_user: VkUser) -> list:
+async def top_3_photos(option_id: int) -> list:
     """
-    Gives next option from vk_user option_list
-    :param vk_user: VkUser
-    :return: option [first_name, last_name, link_to_profile, [photos]]
+    Returns 3 photos with most amount of likes
+    :param: option_id: vk user id.
+    :return: top profile photos
     """
-    ...
+ 
+    photos = await api.photos.get(owner_id=option_id, album_id='profile', extended=1)  
+    photos = photos.items      
+       
+    like_list = []
+    for photo in photos:         
+        like_list.append(photo.likes.count) 
+    like_list.sort()
+    like_list = like_list[-3:]    
+
+    top_photos_list= []
+    for photo in photos:              
+        if photo.likes.count in like_list:
+            top_photos_list.append(f'photo{photo.owner_id}_{photo.id}')
+
+    return top_photos_list 
 
 
 async def search_options(vk_user: VkUser) -> list:
@@ -24,14 +40,37 @@ async def search_options(vk_user: VkUser) -> list:
     Searches options.
     :param: vk_user: VkUser
     :return: list of options
-    """
-    ...
+    """  
+   
+    if vk_user.gender == 2:                        
+        gender = 1                               
+    elif vk_user.gender == 1:
+        gender = 2
+    else:
+        gender = 0 
+   
+    options = await api.users.search(city=vk_user.city, sex=gender, age_from = vk_user.age_from, age_to = vk_user.age_to, status = 6, count = 100, offset=vk_user.offset)
+    vk_user.offset += 100
+    options = options.items   
+    
+    return [option for option in options if not option.is_closed]  
 
 
-async def top_3_photos(option_id: int) -> list:
+async def show_option(vk_user: VkUser) -> list:
     """
-    Returns 3 photos with most amount of likes
-    :param: option_id: vk user id.
-    :return: top profile photos
+    Gives next option from vk_user option_list
+    :param vk_user: VkUser
+    :return: option [first_name, last_name, link_to_profile, [photos]]
     """
-    ...
+
+    if not vk_user.option_list:
+        search_result = await search_options(vk_user)
+        option_list = search_result
+        vk_user.extend_option_list(option_list)
+    option = vk_user.next_option()
+    photos = await top_3_photos(option.id)
+    message = [option.first_name, option.last_name, f'vk.com/id{option.id}', photos]
+    return message
+
+
+# print(asyncio.run(show_option(user_vk)))
--- vk_user.py
+++ vk_user.py
@@ -1,7 +1,9 @@
+# from vkinder_db import add_favorite_to_db, add_photo_to_db, get_favorites
+
 class VkUser:
     user_dict = {}
 
-    def __init__(self, user_id, gender, city):
+    def __init__(self, user_id, gender, city, age_from, age_to):
         """
         :param user_id: user id
         :param gender: gender
@@ -10,40 +12,46 @@ class VkUser:
         self.user_id = user_id
         self.gender = gender
         self.city = city
+        self.age_from = age_from
+        self.age_to = age_to
         self.option_list = []
         self.offset = 0
 
-    def searching_parameters(self) -> list:
-        """
-        Determines parameters for searching
-        :return: list of parameters [city, gender, age_from, age_to]
-        """
-        ...
 
     def extend_option_list(self, options: list):
         """
         Adds more options to self.option_list
         :param options: list of new options
         :return:
-        """
-        ...
+        """   
+        self.option_list.extend(options) 
+              
 
-    def next_option(self):
+    def next_option(self) -> list:
         """
         Gives next option.
         :return: option
         """
-        ...
+        return self.option_list.pop()
+
 
     def add_favorite(self):
         """
         Adds option to favorites
         """
-        ...
+        # add_favorite_to_db()
+        # add_photo_to_db() 
+             
 
-    def show_favorites(self):
+
+    def show_favorites(self) -> list:
         """
         Shows list of favorites options
         """
-        ...
+        # return get_favorites()
+        
 
+
+
+
+# user_vk = VkUser(97600258, 1 , 130, 25, 30)
